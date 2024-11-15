8078cd7a68c7ce34586b5d732452c1c1468356d9
yellobeto <dejesuse1@roverkids.org>
2024-11-15 13:14:46 -0500

    Bug fixes

--- Prototype 1/Assets/Scripts/PlayerController1.cs
+++ Prototype 1/Assets/Scripts/PlayerController1.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController1 : MonoBehaviour
 {  
     // Private Variable
-    private float speed = 5.0f;
-    private float turnSpeed;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f  ;
     private float horizontalInput;
     private float forwardInput;
 
@@ -20,10 +21,10 @@ public class PlayerController1 : MonoBehaviour
         // Where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-
-        // We'll Move our vehicle forward
+        
+        // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); 
-        // We Turn Vehicles
+        // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
